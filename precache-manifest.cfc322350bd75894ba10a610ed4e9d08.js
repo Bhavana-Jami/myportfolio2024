6662336011aa10d968915790f6a3d77523765168
Bhavana Jami <bhavanajami111@gmail.com>
2024-02-11 00:12:34 +0530

    Updates

---
+++ precache-manifest.cfc322350bd75894ba10a610ed4e9d08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d1551eae4a7ce639913bdc8ab98f6a",
+    "url": "/myportfolio2024/index.html"
+  },
+  {
+    "revision": "31f9f3c13c75941feec6",
+    "url": "/myportfolio2024/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "91f1b4df1de9a78c8ec8",
+    "url": "/myportfolio2024/static/css/main.a0426ee5.chunk.css"
+  },
+  {
+    "revision": "31f9f3c13c75941feec6",
+    "url": "/myportfolio2024/static/js/2.9ff81e68.chunk.js"
+  },
+  {
+    "revision": "5ac06fec4121bd76f82f",
+    "url": "/myportfolio2024/static/js/3.35f637f7.chunk.js"
+  },
+  {
+    "revision": "91f1b4df1de9a78c8ec8",
+    "url": "/myportfolio2024/static/js/main.f0621116.chunk.js"
+  },
+  {
+    "revision": "8815ee59f3518b6c50b7",
+    "url": "/myportfolio2024/static/js/runtime~main.a852de2a.js"
+  },
+  {
+    "revision": "5bde6a44b2d521591479e9e546e34c0a",
+    "url": "/myportfolio2024/static/media/my-picture.5bde6a44.jpeg"
+  }
+]);
