7cde8795e77502ea45adc897f8eae4b5230c3fa9
Bhavana Jami <bhavanajami111@gmail.com>
2024-03-10 11:47:23 +0530

    did some fixes in about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = forwardRef((props, ref) => {
     <Container ref={ref} className="d-flex justify-content-center align-items-center flex-column section p-3 gap-4">
       <Row className="section-heading" id="about-heading"><h2>A lil about me</h2></Row>
       <Row className="overlay"><img src={myPic} /></Row>
-      <Row className="about-description">Hey! My name is Brittany and I'm a web developer with a passion for front end development and design. I'm currently a third year student minor in interaction design. I aspire toward a career that will allow me to channel my creativity through crafting beautiful software and engaging experiences.
+      <Row className="about-description">Hey! My name is Brittanyand I'm a web developer with a passion for front end development and design. I'm currently a third year student minor in interaction design. I aspire toward a career that will allow me to channel my creativity through crafting beautiful software and engaging experiences.
 
         When I'm not on the computer, I enjoy snowboarding, swimming, and petting dogs.</Row>
     </Container>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,30 @@
-import React, { forwardRef,useRef } from "react";
+import React, { forwardRef, useRef } from "react";
 import "../styles/Home.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Header from "./Header";
 
 const Home = forwardRef((props, ref) => {
+  
 
   return (
     <Container id="home" ref={ref} className="section">
-     {/* <Header/> */}
-      <Row id="hi" >Hi, I am</Row>
-      <Row id="name" >Bhavana Jami.</Row>
-      <Row id="tagline">I love to create, develop.</Row>
-      {/* <Row id="description">I am currently working in TCS as Associate Software Developer.</Row> */}
-      <Row id="knowmore">Know More..
-        {/* <Col className="knowmore">Know More..</Col> */}
-        {/* <Col className="knowmore">Know More..</Col> */}
-      </Row>
+      {/* <div>  <Header
+        expand="md"
+        homeRef={homeRef}
+        aboutRef={aboutRef}
+        skillsRef={skillsRef}
+        projectsRef={projectsRef}
+      /></div><div> */}
+        <Row id="hi" >Hi, I am</Row>
+        <Row id="name" >Bhavana Jami.</Row>
+        <Row id="tagline">I love to create, develop.</Row>
+        {/* <Row id="description">I am currently working in TCS as Associate Software Developer.</Row> */}
+        <Row id="knowmore">Know More..
+          {/* <Col className="knowmore">Know More..</Col> */}
+          {/* <Col className="knowmore">Know More..</Col> */}
+        </Row>
+      {/* </div> */}
+
     </Container>
   );
 });
