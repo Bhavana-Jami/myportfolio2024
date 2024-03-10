2ca3159d27ab0a218b473b1081da6374905fd6f1
Bhavana Jami <bhavanajami111@gmail.com>
2024-03-10 13:31:09 +0530

    added project details

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
 import "../styles/Projects.css";
 import { Container, Row, Col, Card, Modal, Button } from "react-bootstrap";
 import pic from '../assets/my-picture.jpeg'
@@ -9,82 +9,77 @@ import { RxGithubLogo } from "react-icons/rx";
 
 const Projects = forwardRef((props, ref) => {
   const [modalShow, setModalShow] = React.useState(false);
-  const projects=[{name:"Trigli",techstack:"ReactJs Bootstrap CSS",description:"asjdfgjdsh duh duh dahhh"}]
-  // const project = ()=>{
-  //   projects.map((project))
-  // }
+  const projects = [{ id:1,name: "Portfolio", techstack: "ReactJs ReactBootstrap CSS", description: "This is my portfolio website where I tried to showcase my web developement skills in the aspects of design and dvelopment", github: "git", demo: "demo" },
+  { id:2,name: "Trigli", techstack: "ReactJs ReactBootstrap CSS", description: "Trigli resembles a note taking app except it is specifically developed for the users to journal about their triggers and glimmers", github: "trigligit", demo: "triglidemo" },
+  { id:3,name: "BlueWonk", techstack: "ReactJsReactBootstrap", description: "Blue Wonk is my personal blog where I share various things I am personally interested in.", github: "", demo: "" },
+  { id:4,name: "Bcommerce", techstack: "NA", description: "Coming Soon", github: "NA", demo: "NA" }]
+const [selectedProject, setSelectedProject] = useState(null)
+  const handleModalClick=(id)=>{
+    
+   setModalShow(true)
+  //  console.log(id);
+  return(
+setSelectedProject( projects.filter((project)=>
+    project.id===id
+  )))
+  }
+  console.log(handleModalClick)
   return (
     <>
+    {selectedProject?.map((selecteProject)=>(
+      // <div>{selecteProject.description}</div>
       <Modal
         {...props}
         size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
+        aria-labelledby="contained-modal-title-vcenter "
         centered
         show={modalShow}
         onHide={() => setModalShow(false)}
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
+            {selecteProject.name}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {/* <h4>Centered Modal</h4> */}
           <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
+          {selecteProject.description}
           </p>
         </Modal.Body>
         <Modal.Footer>
-          {/* <Button onClick={props.onHide}>Close</Button> */}
-          {/* <RxGithubLogo/> */}
-          {/* <arr */}
-          <h6>Demo</h6>
-          <h6>Github</h6>
+          <a href={selecteProject.demo}>Demo</a>
+          <a href={selecteProject.github}>Github</a>
         </Modal.Footer>
       </Modal>
-      <div className="section" id="projects-container" style={{ filter: modalShow ? "blur(4px)" :null}}>
-      <div id="project-heading" className="section-heading"><h2>Some of my work </h2><span>more to come ...</span></div>
-      <div
-        ref={ref}
-        id="projects"
-      >
+      
+    ))}
+      
+      <div className="section" id="projects-container" style={{ filter: modalShow ? "blur(4px)" : null }}>
+        <div id="project-heading" className="section-heading"><h2>Some of my work </h2><span>more to come ...</span></div>
+        <div
+          ref={ref}
+          id="projects"
+        >
 
-        <div className="project-card" >
-          {/* <img src={pic} /> */}
-          <div className="cover">
-            <h3>Trigli</h3>
-            <h6>ReactJS, React Bootstrap</h6></div>
-          <SlMagnifierAdd className="magnifier" onClick={() => setModalShow(true)} />
+       
+          {
+            projects.map((project) => (
+              <div className="project-card" >
+                {/* <img src={pic} /> */}
+                <div className="cover">
+                  <h3>{project.name}</h3>
+                  <h6>{project.techstack}</h6></div>
+                <SlMagnifierAdd className="magnifier" onClick={()=>handleModalClick(project.id)} />
+
+              </div>
+            ))
+          }
+         
+
 
         </div>
-        <div className="project-card" >
-          {/* <img src={pic} /> */}
-          <div className="cover">
-            <h3>Trigli</h3>
-            <h6>ReactJS, React Bootstrap</h6></div>
-          <SlMagnifierAdd className="magnifier" onClick={() => setModalShow(true)} />
-
-        </div><div className="project-card" >
-          {/* <img src={pic} /> */}
-          <div className="cover">
-            <h3>Trigli</h3>
-            <h6>ReactJS, React Bootstrap</h6></div>
-          <SlMagnifierAdd className="magnifier" onClick={() => setModalShow(true)} />
-
-        </div><div className="project-card" >
-          {/* <img src={pic} /> */}
-          <div className="cover">
-            <h3>Trigli</h3>
-            <h6>ReactJS, React Bootstrap</h6></div>
-          <SlMagnifierAdd className="magnifier" onClick={() => setModalShow(true)} />
-
-        </div> 
-        
-
-      </div>
-    </div >
+      </div >
     </>
   );
 });
